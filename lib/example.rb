55efa7cc187a9dd2ba52ad64994232500bbec5ac
Larry Hawelu <lhawelu@gmail.com>
2021-04-09 00:12:20 -0700

    commit tree 6adef980a4981e1e296415274a823c757b5f17d6

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,24 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+
+def say_hello_world_ten_time
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+def greeting
+  puts "Hello World"
+end
+
+
+
+
